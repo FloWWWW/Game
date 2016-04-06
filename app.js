d424fe0738e14cbb98427e4b1eadc5f0d1d5c47a
Flo.W <florencewu@FloW-2.local>
2016-04-06 10:15:05 +0800

    init

---
+++ app.js
@@ -0,0 +1,3 @@
+/**
+ * Created by florencewu on 5/4/2016.
+ */
--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, redirect, url_for, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def start():
-    return render_template('canvas.html')
-
-if __name__ == '__main__':
-    app.run()
